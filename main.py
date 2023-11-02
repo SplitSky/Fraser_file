6e3e4bea3333a1a4641f03476b72f0f5fa24bb12
SplitSky <opop.neska60@gmail.com>
2023-11-02 00:14:34 +0000

    it all works now

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-import numpy as np
-import os
+import csv
 
-filename = ""
+# Function to generate a new output CSV file name
+def get_output_filename(base_filename, count):
+    return f"{base_filename}_{count}.csv"
 
-file_out = "Milw_cont_"
-headings = str()
+input_filename = "Milwaukee Contacts - Wired+_With_PK.csv"
+output_base_filename = "output"  # Replace with your desired output base filename
+max_rows_per_file = 9998
 
-with open(filename, 'r') as f:
-    i = 0
-    temp_f = open(file_out + str(i), 'w+');
-    for line in f.readlines():
-        if i % 1000:
-            temp_f.close()
-            temp_f = open(file_out + str(i), 'w+');
-        temp_f.writeline(line)
-    f.close()
+with open(input_filename, 'r', newline='', encoding='iso-8859-1') as input_file:
+    csv_reader = csv.reader(input_file)
+    header = next(csv_reader)  # Read the header row
+
+    line_count = 0
+    file_count = 1
+
+    output_filename = get_output_filename(output_base_filename, file_count)
     
+    # Open the first output file for writing and write the header
+    output_file = open(output_filename, 'w', newline='', encoding='iso-8859-1')
+    csv_writer = csv.writer(output_file)
+    csv_writer.writerow(header)
+
+    for row in csv_reader:
+        csv_writer.writerow(row)
+        line_count += 1
+
+        if line_count >= max_rows_per_file:
+            output_file.close()
+            file_count += 1
+            line_count = 0
+            output_filename = get_output_filename(output_base_filename, file_count)
+            
+            # Open a new output file for writing and write the header
+            output_file = open(output_filename, 'w', newline='', encoding='iso-8859-1')
+            csv_writer = csv.writer(output_file)
+            csv_writer.writerow(header)
+
+    # Close the last output file when the loop is finished
+    output_file.close()
+
+print("CSV files created successfully.")
