886a5cb94cc726f239822bc5f2b611c34e64740a
SplitSky <opop.neska60@gmail.com>
2023-11-03 09:41:52 +0000

    Expanding on the mavenlink code

---
+++ Mavenlink.py
@@ -0,0 +1,98 @@
+import csv
+from datetime import datetime, timedelta
+
+output_base_filename = "output.csv"
+
+def get_week_and_year(date_str):
+    date_obj = datetime.strptime(date_str, "%d/%m/%Y")  # Assuming date format is "MM/DD/YYYY"
+    week_number = date_obj.strftime("%U")  # Get the week number
+    year = date_obj.year
+    return int(week_number), year
+
+def get_monday_of_week(date_str):
+    date_obj = datetime.strptime(date_str, "%d/%m/%Y")  # Assuming date format is "MM/DD/YYYY"
+    monday = date_obj - timedelta(days=date_obj.weekday())  # Calculate Monday by subtracting days
+    return monday.strftime("%d/%m/%Y")
+
+def are_dates_in_same_week_and_year(date_str1, date_str2):
+    # Convert the input date strings to datetime objects using the same format
+    date_obj1 = datetime.strptime(date_str1, "%d/%m/%Y")
+    date_obj2 = datetime.strptime(date_str2, "%d/%m/%Y")
+    # Get the week and year for both dates
+    week1, year1 = get_week_and_year(date_str1)
+    week2, year2 = get_week_and_year(date_str2)
+    # Check if both dates are in the same week and year
+    return week1 == week2 and year1 == year2
+
+
+class dataGroup():
+    def __init__(self, filename):
+        # reads in a csv file and populates it with groups
+        self.fullList = []
+        with open(filename, 'r') as input_file:
+            reader = csv.DictReader(input_file)
+            fieldnames = reader.fieldnames
+            
+            i = 0
+            for row in reader:
+                if i == 0:
+                    print(row.keys())
+                self.check_group(row)
+        
+    def check_groupExists(self, userName, projectName, shareDate):
+        # returns the index or false
+        i = 0
+        for group in self.fullList:
+            if (group.User_Name == userName and group.Project_Name == projectName):
+                # found group > return index
+                if (are_dates_in_same_week_and_year(group.Date_Shared_Created, shareDate)):
+                    # check for date
+                    return i
+            i += 1
+        return False
+        
+    def check_group(self, row):
+        exist = self.check_groupExists(userName=row['User: Name'], projectName=row['Project: Name'], shareDate=row['Date (Shared)'])
+        if (exist == False):
+            self.fullList.append(group(row))
+        else:
+            hoursTemp = float(row['Hours Actual'])
+            self.fullList[exist].Hours_Actual += hoursTemp
+    
+class group(object):
+    def __init__(self,row):
+        self.Time_Entry_ID = row['\ufeffTime Entry: ID']
+        self.Time_Entry_Approved = row['Time Entry: Approved']
+        self.Time_Entry_Billable = row['Time Entry: Billable']
+        self.Time_Entry_Notes = row['Time Entry: Notes']
+        self.Time_Entry_Requires_Approval = row['Time Entry: Requires Approval']
+        self.Time_Entry_Status = row['Time Entry: Status']
+        self.Note = row['Time Entry: Status Note']
+        self.Time_Entry_Taxable = row['Time Entry: Taxable']
+        self.Time_Entry_Type = row['Time Entry: Type']
+        self.User_Name = row['User: Name']
+        self.Project_Name = row['Project: Name']
+        self.Task_Name = row['Task: Name']
+        self.Role = row['Role']
+        self.Currency = row['Currency']
+        self.Location = row['Location']
+        self.Date_Shared = row['Date (Shared)']
+        self.Date_Shared_Created = row['Date (Shared Created)']
+        self.Date_Submission_Submitted = row['Date (Submission Submitted)']
+        self.Date_Submission_Approved = row['Date (Submission Approved)']
+        self.Date_Submission_Rejected = row['Date (Submission Rejected)']
+        self.Hours_Actual = float(row['Hours Actual'])
+        self.Fees_Actual = row['Fees Actual']
+        self.Cost_Actual = row['Cost Actual']
+        self.User_Cost_Rate = row['User Cost Rate']
+        self.Users_Bill_Rate = row['Users Bill Rate (from User Record)']
+        self.TE_Bill_Rate = row['TE Bill Rate']
+        self.TE_Cost_Rate = row['TE Cost Rate']
+        
+
+    
+# Open the input CSV file
+def main():
+    a = dataGroup('Project_Timecard_test.csv')
+    
+main()
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import csv
-
-# Function to generate a new output CSV file name
 def get_output_filename(base_filename, count):
     return f"{base_filename}_{count}.csv"
-
 input_filename = "Milwaukee Contacts - Wired+_With_PK.csv"
 output_base_filename = "output"  # Replace with your desired output base filename
 max_rows_per_file = 9998
@@ -11,13 +8,9 @@ max_rows_per_file = 9998
 with open(input_filename, 'r', newline='', encoding='iso-8859-1') as input_file:
     csv_reader = csv.reader(input_file)
     header = next(csv_reader)  # Read the header row
-
     line_count = 0
     file_count = 1
-
     output_filename = get_output_filename(output_base_filename, file_count)
-    
-    # Open the first output file for writing and write the header
     output_file = open(output_filename, 'w', newline='', encoding='iso-8859-1')
     csv_writer = csv.writer(output_file)
     csv_writer.writerow(header)
@@ -31,13 +24,9 @@ with open(input_filename, 'r', newline='', encoding='iso-8859-1') as input_file:
             file_count += 1
             line_count = 0
             output_filename = get_output_filename(output_base_filename, file_count)
-            
-            # Open a new output file for writing and write the header
             output_file = open(output_filename, 'w', newline='', encoding='iso-8859-1')
             csv_writer = csv.writer(output_file)
             csv_writer.writerow(header)
-
-    # Close the last output file when the loop is finished
     output_file.close()
 
 print("CSV files created successfully.")
